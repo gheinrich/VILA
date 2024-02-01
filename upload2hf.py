413650c222abcbf938896af8103f16059e63a354
Ligeng Zhu <ligeng@mit.edu>
2024-01-31 18:03:26 -0800

    support hash check in hf uploading.

--- upload2hf.py
+++ upload2hf.py
@@ -1,47 +1,60 @@
+import os, os.path as osp
+import argparse
+from hashlib import sha1, sha256
+
 from termcolor import colored
 
-import argparse
-import os, os.path as osp
-from io import BytesIO
+from huggingface_hub import HfApi
+from huggingface_hub.hf_api import CommitOperationAdd
 
-import requests
-import torch
-import transformers
-from transformers import logging
-from PIL import Image
+max_upload_size_per_commit = 16 * 1024 * 1024 * 1024  # 16 GiB
 
-max_upload_size_per_commit = 16 * 1024 * 1024 * 1024 # 16 GiB
+
+def compute_git_hash(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    s = "blob %u\0" % len(data)
+    h = sha1()
+    h.update(s.encode("utf-8"))
+    h.update(data)
+    return h.hexdigest()
+
+
+def compute_lfs_hash(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    h = sha256()
+    h.update(data)
+    return h.hexdigest()
+
 
 if __name__ == "__main__":
     import os
-    os.environ['CURL_CA_BUNDLE'] = ''
+
+    os.environ["CURL_CA_BUNDLE"] = ""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("local_folder", type=str, )
+    parser.add_argument(
+        "local_folder",
+        type=str,
+    )
     parser.add_argument("--model-name", type=str, default=None)
+    parser.add_argument("--hash-check", action="store_true")
     parser.add_argument("--repo-type", type=str, choices=["model", "dataset"])
     parser.add_argument("--repo-org", type=str, default="Efficient-Large-Model")
 
     args = parser.parse_args()
 
-    # eval_model(args)
-    from huggingface_hub import HfApi
-    from huggingface_hub.hf_api import CommitOperationAdd
     api = HfApi()
-    # repo_type="model"
-    # local_folder = "~/downloads/vicuna-13b-clip336-mmc4sub+coyo-finetune-captioner-e4"
+
     repo_type = args.repo_type
     local_folder = args.local_folder
     if local_folder[-1] == "/":
         local_folder = local_folder[:-1]
-    
+
     model_name = osp.basename(local_folder).replace("+", "-")
     repo = osp.join(args.repo_org, model_name)
 
-    # repo_type="dataset"
-    # local_folder = "/home/jasonlu/datasets/coyo-700m/pkl02-split"
-    # repo = "Efficient-Large-Model/coyo-700m-pkl02-split"
-    
     local_folder = os.path.expanduser(local_folder)
     if not api.repo_exists(repo, repo_type=repo_type):
         api.create_repo(
@@ -50,11 +63,12 @@ if __name__ == "__main__":
             repo_type=repo_type,
         )
     print(colored(f"Uploading {osp.join('https://hf.co', repo)}", "green"))
-    
+
     ops = []
     commit_description = ""
     commit_size = 0
     for root, dirs, files in os.walk(local_folder, topdown=True):
+        dirs.sort()
         for name in files:
             fpath = osp.join(root, name)
             rpath = osp.relpath(fpath, local_folder)
@@ -62,12 +76,49 @@ if __name__ == "__main__":
             if "checkpoint-" in rpath:
                 print(colored(f"Checkpoint detected: {rpath}, skipping", "yellow"))
                 continue
-            
+
             if api.file_exists(repo_id=repo, filename=rpath, repo_type=repo_type):
                 # TODO: move to hash check
-                print(colored(f"Already uploaded {rpath}, skipping", "green"))
-                continue
-            
+                if not args.hash_check:
+                    print(
+                        colored(
+                            f"Already uploaded {rpath}, no hash check, skipping",
+                            "green",
+                        )
+                    )
+                    continue
+                else:
+                    hf_meta = list(
+                        api.list_files_info(
+                            repo_id=repo, paths=rpath, repo_type=repo_type
+                        )
+                    )[0]
+
+                    if hf_meta.lfs is not None:
+                        hash_type = "lfs-sha256"
+                        hf_hash = hf_meta.lfs["sha256"]
+                        git_hash = compute_lfs_hash(fpath)
+                    else:
+                        hash_type = "git-sha1"
+                        hf_hash = hf_meta.blob_id
+                        git_hash = compute_git_hash(fpath)
+
+                    if hf_hash == git_hash:
+                        print(
+                            colored(
+                                f"Already uploaded {rpath}, hash check pass, skipping",
+                                "green",
+                            )
+                        )
+                        continue
+                    else:
+                        print(
+                            colored(
+                                f"{rpath} is not same as local version, re-uploading...",
+                                "red",
+                            )
+                        )
+
             operation = CommitOperationAdd(
                 path_or_fileobj=fpath,
                 path_in_repo=rpath,
@@ -78,14 +129,14 @@ if __name__ == "__main__":
             commit_description += f"Upload {rpath}\n"
             if len(ops) <= 8 and commit_size <= max_upload_size_per_commit:
                 continue
-            
+
             commit_message = "Upload files with huggingface_hub"
             commit_info = api.create_commit(
                 repo_id=repo,
                 repo_type=repo_type,
                 operations=ops,
                 commit_message=commit_message,
-                commit_description=commit_description
+                commit_description=commit_description,
             )
             commit_description = ""
             ops = []
@@ -100,5 +151,5 @@ if __name__ == "__main__":
         repo_type=repo_type,
         operations=ops,
         commit_message=commit_message,
-        commit_description=commit_description
+        commit_description=commit_description,
     )
