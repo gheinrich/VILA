16042514f00be9a622f68a9138d9c9b1f4aebec7
Ligeng Zhu <ligeng@mit.edu>
2024-02-01 16:06:48 -0800

    add account config in data_aug scripts.

--- llava/data_aug/reformat_tar.py
+++ llava/data_aug/reformat_tar.py
@@ -6,16 +6,23 @@ import tarfile
 def main(
     src_tar="~/workspace/sa_000000.tar", 
     src_folder="~/workspace",
-    tgt_folder="~/workspace/dev-123"
+    tgt_folder="~/workspace/dev-123",
+    overwrite=False,
 ):
     
     src_tar_path = osp.expanduser(src_tar)
     src_folder_path = osp.expanduser(src_folder)
     tgt_folder_path = osp.expanduser(tgt_folder)
     rpath = osp.relpath(src_tar_path, src_folder_path)
+    fpath = osp.join(tgt_folder_path, rpath)
+    
+    if osp.exists(fpath) and not overwrite:
+        print("Skipping")
+        return 
+        
+    
     t = tarfile.open(src_tar_path)
 
-    fpath = osp.join(tgt_folder_path, rpath)
     os.makedirs(osp.dirname(fpath), exist_ok=True)
     tdev = tarfile.open(fpath, "w")
 
--- llava/data_aug/srun_reformat.sh
+++ llava/data_aug/srun_reformat.sh
@@ -1,17 +1,18 @@
-JOBS_LIMIT=16  # Set your limit here
+JOBS_LIMIT=100  # Set your limit here
 ACCOUNT=llmservice_nlp_fm
 
 src_folder=/lustre/fsw/portfolios/nvr/projects/nvr_elm_llm/dataset/sam-raw
+
 for f in $src_folder/*.tar; do
   while [ $(jobs -rp | wc -l) -ge $JOBS_LIMIT ]; do
-    sleep 1
+    sleep 5
   done
 
   fname=$(echo $f | rev | cut -d "/" -f 1 | rev)
-  echo $fname
   
+  echo "Running $fname,  now total jobs $(jobs -rp | wc -l)"; \
   srun --label -A $ACCOUNT -N 1 \
-    -p cpu,cpu_1,cpu_long -t 4:00:00 \
+    -p cpu,cpu_1,cpu_long -t 1:00:00 \
     -J $ACCOUNT-dev:reformat-$fname \
     -e slurm-logs/dev-split/$fname-$j.err -o slurm-logs/dev-split/$fname-$j.out \
     python llava/data_aug/reformat_tar.py --src_tar=$f --src_folder=$src_folder \
