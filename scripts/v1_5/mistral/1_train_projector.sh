2eaf5b31ca583fca3df4376fbf450dd83d01ffca
Yao Lu <luyao11175@gmail.com>
2024-02-22 01:43:25 -0800

    add missing files

---
+++ scripts/v1_5/mistral/1_train_projector.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source ~/.bashrc
+conda activate vila
+which python
+
+cd ~/workspace/VILA
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
+
+n_node=$SLURM_JOB_NUM_NODES
+bs=$((128 / n_node))
+echo "number of nodes:" $n_node
+echo "per device batch size:" $bs
+echo "node rank:" $SLURM_PROCID
+
+torchrun --nnodes=$n_node --nproc_per_node=8 --master_port=25001 \
+    --master_addr $MASTER_ADDR --node_rank=$SLURM_PROCID \
+    --deepspeed ./scripts/zero2.json \
+    --model_name_or_path /home/jasonlu/models/Mistral-7B-v0.1 \
+    --version plain \
+    --data_mixture ccs_pretrained \
+    --vision_tower google/siglip-so400m-patch14-384 \
+    --mm_projector_type mlp2x_gelu \
+    --tune_mm_mlp_adapter True \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --bf16 True \
+    --output_dir ./checkpoints/vila-mistral-7b-align \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size $bs \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 24000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-3 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
