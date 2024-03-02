8542a82e8b01c036c243891d00745e191d89514c
Seerkfang <895290575@qq.com>
2024-03-02 00:06:34 -0800

    fix evaluation scripts & add SFT data.

--- scripts/v1_5/eval/mmmu.sh
+++ scripts/v1_5/eval/mmmu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MODEL_PATH=$1
 CKPT=$2
+SPLIT=$3
 
 mkdir -p ./playground/data/eval/MMMU/test_results
 
@@ -10,5 +11,5 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_vqa_mmmu \
     --conv-mode vicuna_v1 \
     --config_path llava/eval/mmmu_utils/configs/llava1.5.yaml \
     --output_path ./playground/data/eval/MMMU/test_results/$CKPT.json \
-    --split test
+    --split $SPLIT
 
