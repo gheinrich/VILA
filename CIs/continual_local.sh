e6038680f915aa08ed5e003246d71273b380151c
Ligeng Zhu <ligeng@mit.edu>
2024-03-04 11:47:16 -0800

    add CIs integration test on cluster with email notifications.

--- CIs/continual_local.sh
+++ CIs/continual_local.sh
@@ -1,4 +1,5 @@
-partition=nvr_elm_llm
+# partition=nvr_elm_llm
+partition=llmservice_nlp_fm
 report_file=dev/tmp.md
 
 export VILA_CI_RECIPIENTS="ligengz@nvidia.com"
@@ -10,23 +11,23 @@ for pyfile in tests/cpu_tests/*.py; do
     pylog=${pyfile//\//\-\-}
     > dev/$pylog.err 
     > dev/$pylog.out
-    srun -A nvr_elm_llm \
-        -p cpu -t 4:00:00 -J vila-CI:$pyfile \
+    srun -A $partition \
+        -p cpu,cpu_1,cpu_long -t 4:00:00 -J vila-CI:$pyfile \
         -e dev/$pylog.err -o dev/$pylog.out \
         bash CIs/test_single.sh $pyfile &
 done
-wait
-clear
-cat $report_file
-exit
+# wait
+# clear
+# cat $report_file
+# exit
 
 for pyfile in tests/gpu_tests/*.py; do 
     # bash CIs/test_single.sh $pyfile
     pylog=${pyfile//\//\-\-}
     > dev/$pylog.err 
     > dev/$pylog.out
-    srun -A nvr_elm_llm \
-        -p batch_block1,batch_block2,batch_block3,batch_block4,interactive -t 4:00:00 -J vila-CI:$pyfile \
+    srun -A $partition \
+        -p batch_block1,batch_block2,batch_block3,batch_block4,batch_singlenode -t 4:00:00 -J vila-CI:$pyfile \
         --gpus-per-node 8 --exclusive \
         -e dev/$pylog.err -o dev/$pylog.out \
         bash CIs/test_single.sh $pyfile &
