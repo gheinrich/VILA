2c3b0132568773255ea2e9ac164239bc3927c8a5
Ligeng Zhu <ligeng@mit.edu>
2024-03-04 11:58:23 -0800

    update CIs test example.

--- llava/unit_test_utils.py
+++ llava/unit_test_utils.py
@@ -1,16 +1,18 @@
 from unittest.case import _id as __id, skip as __skip
 
-def requires_gpu():
+def requires_gpu(reason=None):
     import torch
-    reason = "no GPUs detected. Only test in GPU environemnts"
+    reason = "no GPUs detected. Only test in GPU environemnts" if reason is None else reason
     if not torch.cuda.is_available():
         return __skip(reason)
     return __id
 
 
-def requires_lustre():
+def requires_lustre(reason=None):
     import os.path as osp
     if not osp.isdir("/lustre"):
-        reason = "lustre path is not avaliable."
+        reason = "lustre path is not avaliable." if reason is None else reason
         return __skip(reason)
     return __id
+
+
--- tests/gpu_tests/sample_gpu_test.py
+++ tests/gpu_tests/sample_gpu_test.py
@@ -3,27 +3,43 @@ import unittest
 from llava.unit_test_utils import requires_lustre, requires_gpu
 
 class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    @unittest.expectedFailure
+    def test_expected_failure(self):
+        a = []
+        print(a[5])
     
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
     @requires_gpu()
     def test_gpu_funcs(self):
         import torch
         a = torch.randn(3).cuda()
         b = torch.randn(3).cuda()
         print(a + b)
+        
+    @requires_lustre()
+    def test_lustre_access(self):
+        from llava.data.simple_vila_webdataset import VILAWebDataset
+        train_dataset = VILAWebDataset(
+            data_path="/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/datasets/captioning/coyo-25m-vila",
+        )
+        print(train_dataset[0])
+        
+    
+    @requires_gpu()
+    @requires_lustre()
+    def test_both(self):
+        from llava.data.simple_vila_webdataset import VILAWebDataset
+        train_dataset = VILAWebDataset(
+            data_path="/lustre/fsw/portfolios/llmservice/projects/llmservice_nlp_fm/datasets/captioning/coyo-25m-vila",
+        )
+        print(train_dataset[0])
+        import torch
+        a = torch.randn(3).cuda()
+        b = torch.randn(3).cuda()
+        print(a + b)
+        
+        
+    
+        
     
     
 if __name__ == '__main__':
