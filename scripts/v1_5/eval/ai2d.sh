c7c1c911a4fafa76a08749a19709f0889d45316a
Seerkfang <895290575@qq.com>
2024-03-26 20:23:54 -0700

    minor fix for ai2d & chartqa evaluation.

--- scripts/v1_5/eval/ai2d.sh
+++ scripts/v1_5/eval/ai2d.sh
@@ -6,4 +6,5 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.evaluate_vqa \
     --model-path $MODEL_PATH \
     --image-folder ./playground/data/eval/ai2d \
     --dataset ai2diagram_test \
-    --conv-mode vicuna_v1
+    --conv-mode vicuna_v1 \
+    --answer-dir ./eval_output/$CKPT/ai2d/answers
--- scripts/v1_5/eval/chartqa.sh
+++ scripts/v1_5/eval/chartqa.sh
@@ -6,10 +6,12 @@ CUDA_VISIBLE_DEVICES=0 python -m llava.eval.evaluate_vqa \
     --model-path $MODEL_PATH \
     --image-folder ./playground/data/eval/chartqa \
     --dataset  chartqa_test_human \
-    --conv-mode vicuna_v1
+    --conv-mode vicuna_v1 \
+    --answer-dir ./eval_output/$CKPT/chartqa/answers
 
 CUDA_VISIBLE_DEVICES=0 python -m llava.eval.evaluate_vqa \
     --model-path $MODEL_PATH \
     --image-folder ./playground/data/eval/chartqa \
     --dataset  chartqa_test_augmented \
-    --conv-mode vicuna_v1
+    --conv-mode vicuna_v1 \
+    --answer-dir ./eval_output/$CKPT/chartqa/answers
