2ac16505544b62da117915b24ddbedcf8fd2950d
Ligeng Zhu <ligeng@mit.edu>
2024-03-27 13:57:02 -0700

    update code

--- llava/mm_utils.py
+++ llava/mm_utils.py
@@ -9,16 +9,18 @@ from llava.constants import IMAGE_TOKEN_INDEX
 import tempfile
 from io import BytesIO
 
+
 def get_frame_from_vcap(vidcap, num_frames=10):
     import cv2
+
     fps = vidcap.get(cv2.CAP_PROP_FPS)
-    
+
     if fps == 0:
         print("Video file not found")
         return [
-             Image.new('RGB', (720, 720)),
+            Image.new("RGB", (720, 720)),
         ]
-    
+
     frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     duration = frame_count / fps
 
@@ -40,6 +42,7 @@ def get_frame_from_vcap(vidcap, num_frames=10):
 
     return images
 
+
 def opencv_extract_frames(vpath_or_bytesio, frames=6):
     """
     Extract frames from a video using OpenCV.
@@ -55,19 +58,19 @@ def opencv_extract_frames(vpath_or_bytesio, frames=6):
         NotImplementedError: If the type of `vpath_or_bytesio` is not supported.
     """
     import cv2
+
     if isinstance(vpath_or_bytesio, str):
         vidcap = cv2.VideoCapture(vpath_or_bytesio)
         return get_frame_from_vcap(vidcap, frames)
     elif isinstance(vpath_or_bytesio, (BytesIO,)):
         # assuming mp4
-        with tempfile.NamedTemporaryFile(delete=True, suffix='.mp4') as temp_video:
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".mp4") as temp_video:
             temp_video.write(vpath_or_bytesio.read())
             temp_video_name = temp_video.name
             vidcap = cv2.VideoCapture(temp_video_name)
             return get_frame_from_vcap(vidcap, frames)
     else:
         raise NotImplementedError(type(vpath_or_bytesio))
-
 
 
 def load_image_from_base64(image):
@@ -102,71 +105,40 @@ def expand2square(pil_img, background_color):
         return result
 
 
-def sample_from_video(video_path, num_video_frames):
-    """
-    Extracts a specified number of frames from a video file.
-
-    Args:
-        video_path (str): The path to the video file.
-        num_video_frames (int): The number of frames to extract from the video.
-
-    Returns:
-        list: A list of PIL.Image objects representing the extracted frames.
-    """
-    import cv2
-    from PIL import Image
-
-    vidcap = cv2.VideoCapture(video_path)
-    fps = vidcap.get(cv2.CAP_PROP_FPS)
-    frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-    duration = frame_count / fps
-
-    frame_interval = frame_count // 10
-
-    def get_frame(max_frames):
-        images = []
-        count = 0
-        success = True
-        while success:
-            success, frame = vidcap.read()
-            if count % frame_interval:
-                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                im_pil = Image.fromarray(img)
-                images.append(im_pil)
-                if len(images) == max_frames:
-                    return images
-
-            count += 1
-
-        return images
-
-    return get_frame(num_video_frames)
-
-
 def process_images(images, image_processor, model_cfg):
     image_aspect_ratio = getattr(model_cfg, "image_aspect_ratio", None)
     new_images = []
-    if image_aspect_ratio == 'pad':
+    if image_aspect_ratio == "pad":
         for image in images:
-            image = expand2square(image, tuple(int(x*255) for x in image_processor.image_mean))
-            image = image_processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            image = expand2square(
+                image, tuple(int(x * 255) for x in image_processor.image_mean)
+            )
+            image = image_processor.preprocess(image, return_tensors="pt")[
+                "pixel_values"
+            ][0]
             new_images.append(image)
     else:
-        return image_processor(images, return_tensors='pt')['pixel_values']
+        return image_processor(images, return_tensors="pt")["pixel_values"]
     if all(x.shape == new_images[0].shape for x in new_images):
         new_images = torch.stack(new_images, dim=0)
     return new_images
 
 
-def tokenizer_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX, return_tensors=None):
-    prompt_chunks = [tokenizer(chunk).input_ids for chunk in prompt.split('<image>')]
+def tokenizer_image_token(
+    prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX, return_tensors=None
+):
+    prompt_chunks = [tokenizer(chunk).input_ids for chunk in prompt.split("<image>")]
 
     def insert_separator(X, sep):
-        return [ele for sublist in zip(X, [sep]*len(X)) for ele in sublist][:-1]
+        return [ele for sublist in zip(X, [sep] * len(X)) for ele in sublist][:-1]
 
     input_ids = []
     offset = 0
-    if len(prompt_chunks) > 0 and len(prompt_chunks[0]) > 0 and prompt_chunks[0][0] == tokenizer.bos_token_id:
+    if (
+        len(prompt_chunks) > 0
+        and len(prompt_chunks[0]) > 0
+        and prompt_chunks[0][0] == tokenizer.bos_token_id
+    ):
         offset = 1
         input_ids.append(prompt_chunks[0][0])
 
@@ -174,9 +146,9 @@ def tokenizer_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX
         input_ids.extend(x[offset:])
 
     if return_tensors is not None:
-        if return_tensors == 'pt':
+        if return_tensors == "pt":
             return torch.tensor(input_ids, dtype=torch.long)
-        raise ValueError(f'Unsupported tensor type: {return_tensors}')
+        raise ValueError(f"Unsupported tensor type: {return_tensors}")
     return input_ids
 
 
@@ -187,7 +159,7 @@ def is_gemma_tokenizer(tokenizer):
 def get_model_name_from_path(model_path):
     model_path = model_path.strip("/")
     model_paths = model_path.split("/")
-    if model_paths[-1].startswith('checkpoint-'):
+    if model_paths[-1].startswith("checkpoint-"):
         return model_paths[-2] + "_" + model_paths[-1]
     else:
         return model_paths[-1]
@@ -200,27 +172,38 @@ class KeywordsStoppingCriteria(StoppingCriteria):
         self.max_keyword_len = 0
         for keyword in keywords:
             cur_keyword_ids = tokenizer(keyword).input_ids
-            if len(cur_keyword_ids) > 1 and cur_keyword_ids[0] == tokenizer.bos_token_id:
+            if (
+                len(cur_keyword_ids) > 1
+                and cur_keyword_ids[0] == tokenizer.bos_token_id
+            ):
                 cur_keyword_ids = cur_keyword_ids[1:]
             if len(cur_keyword_ids) > self.max_keyword_len:
                 self.max_keyword_len = len(cur_keyword_ids)
             self.keyword_ids.append(torch.tensor(cur_keyword_ids))
         self.tokenizer = tokenizer
         self.start_len = input_ids.shape[1]
-    
-    def call_for_batch(self, output_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+
+    def call_for_batch(
+        self, output_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
+    ) -> bool:
         offset = min(output_ids.shape[1] - self.start_len, self.max_keyword_len)
-        self.keyword_ids = [keyword_id.to(output_ids.device) for keyword_id in self.keyword_ids]
+        self.keyword_ids = [
+            keyword_id.to(output_ids.device) for keyword_id in self.keyword_ids
+        ]
         for keyword_id in self.keyword_ids:
-            if (output_ids[0, -keyword_id.shape[0]:] == keyword_id).all():
+            if (output_ids[0, -keyword_id.shape[0] :] == keyword_id).all():
                 return True
-        outputs = self.tokenizer.batch_decode(output_ids[:, -offset:], skip_special_tokens=True)[0]
+        outputs = self.tokenizer.batch_decode(
+            output_ids[:, -offset:], skip_special_tokens=True
+        )[0]
         for keyword in self.keywords:
             if keyword in outputs:
                 return True
         return False
-    
-    def __call__(self, output_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+
+    def __call__(
+        self, output_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
+    ) -> bool:
         outputs = []
         for i in range(output_ids.shape[0]):
             outputs.append(self.call_for_batch(output_ids[i].unsqueeze(0), scores))
--- tests/cpu_tests/dataset_panda70m.py
+++ tests/cpu_tests/dataset_panda70m.py
@@ -4,7 +4,7 @@ from llava.unit_test_utils import requires_gpu, requires_lustre, test_make_super
 class TestStringMethods(unittest.TestCase):
     @requires_lustre()
     def test_dataloader_panda70m(self):
-        test_make_supervised_data_module(dataset_name="panda70m", batch_size=32)
+        test_make_supervised_data_module(dataset_name="panda70m", batch_size=32, num_workers=32)
 
 
 if __name__ == "__main__":
