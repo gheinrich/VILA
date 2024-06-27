1785bc988d10dc471b2212b46eb3359e65ef156c
Ligeng Zhu <ligeng@mit.edu>
2024-06-27 00:17:35 -0700

    add haotian and zhijian to ci maillist

--- CIs/integrate.sh
+++ CIs/integrate.sh
@@ -1,7 +1,7 @@
 # /bin/bash
 export SLURM_ACCOUNT=nvr_elm_llm
 
-VILA_CI_MAILIST="ligengz@nvidia.com,jasonlu@nvidia.com,yunhaof@nvidia.com,fuzhaox@nvidia.com,yukangc@nvidia.com,huqinghao@gmail.com"
+VILA_CI_MAILIST="ligengz@nvidia.com,jasonlu@nvidia.com,yunhaof@nvidia.com,fuzhaox@nvidia.com,yukangc@nvidia.com,huqinghao@gmail.com,haotiant@nvidia.com,zhijianl@nvidia.com"
 
 SECONDS=0
 while true; do
