5d55a156be2873244a84e68fa22062877a54d94d
Yao Lu <91149044+yaolug@users.noreply.github.com>
2024-07-10 07:25:41 -0700

    Update run_vila_benchmark.sh (#129)

--- scripts/v1_5/eval/video_chatgpt/run_vila_benchmark.sh
+++ scripts/v1_5/eval/video_chatgpt/run_vila_benchmark.sh
@@ -5,7 +5,7 @@ model_name=$2
 conv_mode=$3
 
 echo vila_benchmark
-output_dir="./eval_output/VILA-benchmark/${model_name}"
+output_dir="./eval_output/${model_name}/VILA-benchmark"
 python llava/eval/video/model_vqa_videodemo_benchmark.py --model-path ${model_path} --eval_type pexels --output_dir ${output_dir} --conv-mode ${conv_mode} --temperature 0
 python llava/eval/video/model_vqa_videodemo_benchmark.py --model-path ${model_path} --eval_type robotics --output_dir ${output_dir} --conv-mode ${conv_mode} --temperature 0
 python llava/eval/video/model_vqa_videodemo_benchmark.py --model-path ${model_path} --eval_type av --output_dir ${output_dir} --conv-mode ${conv_mode} --temperature 0
