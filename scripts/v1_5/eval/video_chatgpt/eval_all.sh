017a656e2d65cf2cc6825a21f1a926407fe78bc4
yukang <yukangchen@cse.cuhk.edu.hk>
2024-07-12 02:39:05 +0800

    fix bug in output dir in VILA benchmark (#131)
    * Update eval_all.sh
    
    * Update eval_vila_benchmark_gpt4.sh

--- scripts/v1_5/eval/video_chatgpt/eval_all.sh
+++ scripts/v1_5/eval/video_chatgpt/eval_all.sh
@@ -29,25 +29,25 @@ python llava/eval/video_mme/mme_calc.py --results_file ./eval_output/$model_name
 echo vila_benchmark
 echo pexels
 output_dir="./eval_output/${model_name}/VILA-benchmark/pexels"
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} correctness ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} detailed_orientation ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} context ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} pexels correctness ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} pexels detailed_orientation ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} pexels context ${output_dir}/pred.json
 
 output_dir="./eval_output/${model_name}/VILA-benchmark/robotics"
 echo robotics
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} correctness ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} detailed_orientation ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} context ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} robotics correctness ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} robotics detailed_orientation ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} robotics context ${output_dir}/pred.json
 
 output_dir="./eval_output/${model_name}/VILA-benchmark/av"
 echo av
 
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} correctness ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} detailed_orientation ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} context ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} av correctness ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} av detailed_orientation ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} av context ${output_dir}/pred.json
 
 output_dir="./eval_output/${model_name}/VILA-benchmark/long"
 echo cartoon
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} correctness ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} detailed_orientation ${output_dir}/pred.json
-bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} context ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} cartoon correctness ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} cartoon detailed_orientation ${output_dir}/pred.json
+bash ./scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh ${model_name} cartoon context ${output_dir}/pred.json
--- scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh
+++ scripts/v1_5/eval/video_chatgpt/eval_vila_benchmark_gpt4.sh
@@ -1,13 +1,14 @@
 MODEL=$1
-TYPE=$2
-pred_path=$3
+DATA_TYPE=$2
+EVAL_TYPE=$3
+pred_path=$4
 Video_5_Benchmark="eval_vila_benchmark"
-output_dir="${Video_5_Benchmark}/${MODEL}/gpt4/${TYPE}"
-output_json="${Video_5_Benchmark}/${MODEL}/results/${TYPE}_qa.json"
+output_dir="${Video_5_Benchmark}/${MODEL}/${DATA_TYPE}/gpt4/${EVAL_TYPE}"
+output_json="${Video_5_Benchmark}/${MODEL}/${DATA_TYPE}/results/${EVAL_TYPE}_qa.json"
 
-mkdir -p "${Video_5_Benchmark}/${MODEL}/results/"
+mkdir -p "${Video_5_Benchmark}/${MODEL}/${DATA_TYPE}/results/"
 
-python llava/eval/video/eval_benchmark_${TYPE}.py \
+python llava/eval/video/eval_benchmark_${EVAL_TYPE}.py \
     --pred_path  ${pred_path} \
     --output_dir  ${output_dir} \
     --output_json  ${output_json} \
