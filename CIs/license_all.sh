616dbe7fa8aff0338a0bd55a0d870bc537d74c94
Ligeng Zhu <Lyken17@users.noreply.github.com>
2024-07-26 08:05:00 +0800

    Add License for LongVILA Legal (#143)

--- CIs/license_all.sh
+++ CIs/license_all.sh
@@ -1,1 +1,1 @@
-addlicense -s -c 'NVIDIA CORPORATION & AFFILIATES' -ignore "llava/eval/**"  -ignore "**/*__init__.py"  **/*.py
+addlicense -s -c 'NVIDIA CORPORATION & AFFILIATES' -ignore "llava/eval/**" -ignore "llava/wids/**" -ignore "**/*__init__.py" **/*.py
--- llava/model/language_model/mpt/flash_attn_triton.py
+++ llava/model/language_model/mpt/flash_attn_triton.py
@@ -1,3 +1,19 @@
+# Copyright 2024 NVIDIA CORPORATION & AFFILIATES
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 """
 Copied from https://github.com/HazyResearch/flash-attention/blob/eff9fe6b8076df59d64d7a3f464696738a3c7c24/flash_attn/flash_attn_triton.py
 update imports to use 'triton_pre_mlir'
