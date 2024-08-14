4956922fe8d7654c03fc31aa4fed7b7d2bf53b49
Zhijian Liu <5782437+zhijian-liu@users.noreply.github.com>
2024-08-14 19:40:27 -0400

    Revert "Remove `transformer_replace`" (#161)

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -15,40 +15,28 @@
 # This file is modified from https://github.com/haotian-liu/LLaVA/
 
 
+import inspect
 import os
-from functools import partial
 from typing import List, Optional, Tuple, Union
-from unittest.mock import patch
 
 import torch
-from torch.nn import functional as F
 from transformers import AutoConfig, AutoModel, PretrainedConfig, PreTrainedModel
 from transformers.modeling_outputs import CausalLMOutputWithPast
-
-from llava.utils import distributed as dist
 
 from ...train.utils import calculate_loss_weight
 from ..configuration_llava import LlavaConfig
 from ..llava_arch import LlavaMetaForCausalLM, LlavaMetaModel
 
 
-def _get_unpad_data(attention_mask, _seqlens_in_batch, *args, **kwargs):
-    indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
-    max_seqlen_in_batch = _seqlens_in_batch.max().item()
-    cu_seqlens = F.pad(torch.cumsum(_seqlens_in_batch, dim=0, dtype=torch.int32), (1, 0))
-    return (indices, cu_seqlens, max_seqlen_in_batch)
-
-
 class LlavaLlamaConfig(LlavaConfig):
     model_type = "llava_llama"
 
 
-# FIXME we will follow the convention to add a new class for CausalLM in the future
+## FIXME we will follow the convention to add a new class for CausalLM in the future
 class LlavaLlamaModel(LlavaMetaModel, LlavaMetaForCausalLM, PreTrainedModel):
     config_class = LlavaLlamaConfig
     main_input_name = "input_embeds"
     supports_gradient_checkpointing = True
-    _supports_flash_attn_2 = True
 
     def __init__(self, config: LlavaLlamaConfig = None, *args, **kwargs) -> None:
         super().__init__(config)
@@ -104,15 +92,16 @@ class LlavaLlamaModel(LlavaMetaModel, LlavaMetaForCausalLM, PreTrainedModel):
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[List[torch.FloatTensor]] = None,
+        seqlens_in_batch: Optional[torch.LongTensor] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
         labels: Optional[torch.LongTensor] = None,
-        packing: bool = True,
-        seqlens_in_batch: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
         dpo_forward: bool = False,
-        **kwargs,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         self.freezed_module_patch()
-
         if inputs_embeds is None:
             (
                 input_ids,
@@ -125,50 +114,71 @@ class LlavaLlamaModel(LlavaMetaModel, LlavaMetaForCausalLM, PreTrainedModel):
                 input_ids, position_ids, attention_mask, past_key_values, labels, images
             )
 
-        if packing and self.training and not dpo_forward:
+        support_packing = "seqlens_in_batch" in inspect.signature(self.llm.forward).parameters
+
+        if self.training and support_packing and not dpo_forward:
             (
                 _,
+                new_position_ids,
+                new_attention_mask,
+                _,
+                new_inputs_embeds,
+                new_labels,
+                sorted_seqlens_in_batch,
+            ) = self.repack_multimodal_data(
+                input_ids,
                 position_ids,
                 attention_mask,
-                _,
+                past_key_values,
                 inputs_embeds,
                 labels,
-                _seqlens_in_batch,
-            ) = self.repack_multimodal_data(
-                input_ids, position_ids, attention_mask, past_key_values, inputs_embeds, labels
             )
-            if _seqlens_in_batch is None:
-                _seqlens_in_batch = seqlens_in_batch
-
-            wraps = partial(_get_unpad_data, _seqlens_in_batch=_seqlens_in_batch)
-            with patch(self.llm.__module__ + "._get_unpad_data", wraps=wraps) as m:
-                outputs = self.llm(
-                    inputs_embeds=inputs_embeds,
-                    attention_mask=attention_mask,
-                    position_ids=position_ids,
-                    labels=labels,
-                    **kwargs,
-                )
-                if 0 in attention_mask and m.call_count == 0:
-                    raise ValueError("The forward function of the model should call `_get_unpad_data` function.")
+            if sorted_seqlens_in_batch is None:
+                sorted_seqlens_in_batch = seqlens_in_batch
+            new_input_ids = None
+            past_key_values = None
         else:
-            outputs = self.llm(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                position_ids=position_ids,
+            new_attention_mask = attention_mask
+            new_position_ids = position_ids
+            new_inputs_embeds = inputs_embeds
+            new_labels = labels
+            sorted_seqlens_in_batch = attention_mask.sum(-1).int()
+            new_input_ids = input_ids
+
+        if support_packing:
+            outputs = self.llm.forward(
+                input_ids=new_input_ids,
+                attention_mask=new_attention_mask,
+                position_ids=new_position_ids,
                 past_key_values=past_key_values,
-                inputs_embeds=inputs_embeds,
-                labels=labels,
-                **kwargs,
+                inputs_embeds=new_inputs_embeds,
+                labels=new_labels,
+                use_cache=use_cache,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+                seqlens_in_batch=sorted_seqlens_in_batch,
+            )
+        else:
+            outputs = self.llm.forward(
+                input_ids=new_input_ids,
+                attention_mask=new_attention_mask,
+                position_ids=new_position_ids,
+                past_key_values=past_key_values,
+                inputs_embeds=new_inputs_embeds,
+                labels=new_labels,
+                use_cache=use_cache,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
             )
 
         # Loss rescale for SP & DP loss match
-        if dist.size() > 1:
-            loss_weight = calculate_loss_weight(labels)
-            outputs.loss = outputs.loss * loss_weight
+        loss_weight = calculate_loss_weight(new_labels)
+        outputs.loss = outputs.loss * loss_weight
 
         if dpo_forward:
-            return outputs.logits, labels
+            return outputs.logits, new_labels
         return outputs
 
 
--- tests/python/test_packing.py
+++ tests/python/test_packing.py
@@ -14,10 +14,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import copy
 import unittest
 
 import torch
-from parameterized import parameterized
 from transformers import AutoTokenizer
 
 from llava.model import LlavaLlamaConfig, LlavaLlamaModel
@@ -31,16 +31,14 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
-MODELS = ["lmsys/vicuna-7b-v1.5", "Qwen/Qwen2-7B"]
-
 
 class TestInputPacking(unittest.TestCase):
-    @parameterized.expand(MODELS)
-    def test_loss_close(self, model_name_or_path: str):
+    def setUp(self):
+        # This test is supposed to run on a single GPU
         if torch.cuda.is_available():
             rank = 0
             torch.cuda.set_device(rank)
-
+        model_name_or_path = "lmsys/vicuna-7b-v1.5"
         self.model_args = ModelArguments(
             model_name_or_path=model_name_or_path,
             version="v1",
@@ -50,10 +48,10 @@ class TestInputPacking(unittest.TestCase):
             mm_use_im_patch_token=False,
         )
         self.data_args = DataArguments()
-        self.training_args = TrainingArguments(output_dir="", bf16=True)
+        self.training_args = TrainingArguments(output_dir="")
         self.config = LlavaLlamaConfig.from_pretrained(model_name_or_path)
         prepare_config_for_training(self.config, self.model_args, self.training_args, self.data_args)
-
+        print("Initializing tokenizer...")
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_name_or_path,
             model_max_length=4096,
@@ -61,30 +59,91 @@ class TestInputPacking(unittest.TestCase):
             use_fast=False,
             legacy=False,
         )
+        print("Initializing LlavaLlamaModel...")
+        self.model = LlavaLlamaModel(config=self.config)
+        self.model.vision_tower = self.model.vision_tower.to(device)
+        self.model.mm_projector = self.model.mm_projector.to(device)
+        self.model = self.model.to(device)
 
-        model = LlavaLlamaModel(config=self.config, attn_implementation="flash_attention_2")
-        model.vision_tower = model.vision_tower.to(device)
-        model.mm_projector = model.mm_projector.to(device)
-        model = model.to(device)
-        model.llm.pad_token_id = self.tokenizer.pad_token_id
-
+        print("Initializing data...")
         data = torch.load("tests/sample_data/test_packing.pth")
+        # necessary for model forward
+        self.model.llm.pad_token_id = self.tokenizer.pad_token_id
+        self.data = data
+
+    # @requires_gpu()
+    def test_loss_close(self):
+        print("Preprocessing inputs...")
+        data = copy.deepcopy(self.data)
         data["input_ids"] = data["input_ids"].to(device)
-        data["images"] = data["images"].to(torch.bfloat16).to(device)
+        data["images"] = data["images"].to(torch.float16).to(device)
         data["attention_mask"] = data["attention_mask"].to(device)
         data["labels"] = data["labels"].to(device)
+        data["position_ids"] = None
+        data["past_key_values"] = None
 
-        model.train()
+        (
+            input_ids,
+            position_ids,
+            attention_mask,
+            past_key_values,
+            inputs_embeds,
+            labels,
+        ) = self.model.prepare_inputs_labels_for_multimodal(**data)
 
-        output = model(**data, packing=True, use_cache=False)
-        output_loss = output.loss.item()
-        del output
+        print("Packing inputs...")
+        (
+            _,
+            new_position_ids,
+            new_attention_mask,
+            _,
+            new_inputs_embeds,
+            new_labels,
+            sorted_seqlens_in_batch,
+        ) = self.model.repack_multimodal_data(
+            input_ids,
+            position_ids,
+            attention_mask,
+            past_key_values,
+            inputs_embeds,
+            labels,
+        )
 
-        target = model(**data, packing=False, use_cache=False)
-        target_loss = target.loss.item()
-        del target
+        print("Running models...")
 
-        self.assertAlmostEqual(output_loss, target_loss, places=2)
+        with torch.no_grad():
+            # forward results with input packing
+            outputs = self.model.llm.forward(
+                input_ids=None,
+                attention_mask=new_attention_mask,
+                position_ids=new_position_ids,
+                past_key_values=None,
+                inputs_embeds=new_inputs_embeds,
+                labels=new_labels,
+                use_cache=False,
+                output_attentions=True,
+                output_hidden_states=True,
+                return_dict=True,
+                seqlens_in_batch=sorted_seqlens_in_batch,
+            )
+            # forward results without input packing
+            outputs_ref = self.model.llm.forward(
+                input_ids=None,
+                attention_mask=attention_mask.to(device),
+                position_ids=None,
+                past_key_values=None,
+                inputs_embeds=inputs_embeds,
+                labels=labels,
+                use_cache=False,
+                output_attentions=True,
+                output_hidden_states=True,
+                return_dict=True,
+            )
+            # loss should be very similar (but not the same due to numerical precision issues)
+            loss = outputs.loss
+            loss_ref = outputs_ref.loss
+            print("loss =", loss, "loss_ref =", loss_ref)
+            self.assertAlmostEqual(loss.item(), loss_ref.item(), places=2)
 
 
 if __name__ == "__main__":
