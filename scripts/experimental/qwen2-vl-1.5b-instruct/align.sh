a6cbdb8198d50f6fceea53ce9b5ddad7a48769bd
Zhijian Liu <5782437+zhijian-liu@users.noreply.github.com>
2024-09-01 12:09:07 -0400

    Add scripts for Qwen2 1.5B and VL models (#213)

---
+++ scripts/experimental/qwen2-vl-1.5b-instruct/align.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DEFAULT_RUN_NAME="vila-qwen2-vl-1.5b-instruct-align"
+DEFAULT_GLOBAL_TRAIN_BATCH_SIZE=2048
+DEFAULT_GRADIENT_ACCUMULATION_STEPS=1
+
+source scripts/setups/train.sh
+
+torchrun \
+    --nnodes=$NNODES --nproc_per_node=$GPUS_PER_NODE --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
+    llava/train/train_mem.py \
+        --deepspeed scripts/zero3.json \
+        --model_name_or_path Efficient-Large-Model/Qwen2-VL-1.5B-Instruct \
+        --chat_template qwen2 \
+        --data_mixture llava_1_5_mm_align \
+        --vision_tower google/siglip-so400m-patch14-384 \
+        --mm_vision_select_feature cls_patch \
+        --mm_projector mlp_downsample \
+        --tune_vision_tower False \
+        --tune_mm_projector True \
+        --tune_language_model False \
+        --mm_vision_select_layer -2 \
+        --mm_use_im_start_end False \
+        --mm_use_im_patch_token False \
+        --image_aspect_ratio resize \
+        --bf16 True \
+        --output_dir $OUTPUT_DIR/model \
+        --num_train_epochs 1 \
+        --per_device_train_batch_size $PER_DEVICE_TRAIN_BATCH_SIZE \
+        --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
+        --evaluation_strategy no \
+        --save_strategy steps \
+        --save_steps 100 \
+        --save_total_limit 1 \
+        --learning_rate 1e-3 \
+        --weight_decay 0. \
+        --warmup_ratio 0.03 \
+        --lr_scheduler_type cosine \
+        --logging_steps 1 \
+        --model_max_length 4096 \
+        --gradient_checkpointing True \
+        --dataloader_num_workers 8 \
+        --report_to wandb
---
+++ scripts/experimental/qwen2-vl-7b-instruct/pretrain.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DEFAULT_RUN_NAME="vila-qwen2-vl-7b-pretrain"
+DEFAULT_GLOBAL_TRAIN_BATCH_SIZE=1024
+DEFAULT_GRADIENT_ACCUMULATION_STEPS=1
+
+source scripts/setups/train.sh
+
+STAGE1_PATH=$1
+
+torchrun \
+    --nnodes=$NNODES --nproc_per_node=$GPUS_PER_NODE --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
+    llava/train/train_mem.py \
+        --deepspeed scripts/zero3.json \
+        --model_name_or_path $STAGE1_PATH \
+        --data_mixture sharegpt4v_pretrain \
+        --vision_tower google/siglip-so400m-patch14-384 \
+        --mm_vision_select_feature cls_patch \
+        --mm_projector mlp_downsample \
+        --tune_vision_tower False \
+        --tune_mm_projector True \
+        --tune_language_model True \
+        --mm_vision_select_layer -2 \
+        --mm_use_im_start_end False \
+        --mm_use_im_patch_token False \
+        --image_aspect_ratio resize \
+        --bf16 True \
+        --output_dir $OUTPUT_DIR/model \
+        --num_train_epochs 1 \
+        --per_device_train_batch_size $PER_DEVICE_TRAIN_BATCH_SIZE \
+        --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
+        --evaluation_strategy no \
+        --save_strategy steps \
+        --save_steps 100 \
+        --save_total_limit 1 \
+        --learning_rate 5e-5 \
+        --weight_decay 0. \
+        --warmup_ratio 0.03 \
+        --lr_scheduler_type cosine \
+        --logging_steps 1 \
+        --model_max_length 4096 \
+        --gradient_checkpointing True \
+        --dataloader_num_workers 4 \
+        --report_to wandb
