7e359ff6c1af90d216fff91447ecf59e02f6e251
Zhijian Liu <5782437+zhijian-liu@users.noreply.github.com>
2024-09-24 02:52:49 -0400

    Add scripts for `qwen2.5-7b-instruct` (#251)

---
+++ scripts/experimental/qwen2.5-7b-instruct/sft.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+DEFAULT_RUN_NAME="vila-qwen2.5-7b-instruct-sft"
+DEFAULT_GLOBAL_TRAIN_BATCH_SIZE=2048
+DEFAULT_GRADIENT_ACCUMULATION_STEPS=2
+
+source scripts/setups/train.sh
+
+STAGE2_PATH=$1
+
+torchrun \
+    --nnodes=$NNODES --nproc_per_node=$GPUS_PER_NODE --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
+    llava/train/train_mem.py \
+        --deepspeed scripts/zero3.json \
+        --model_name_or_path $STAGE2_PATH \
+        --data_mixture vila-v1.5-sft \
+        --vision_tower google/siglip-so400m-patch14-384 \
+        --mm_vision_select_feature cls_patch \
+        --mm_projector mlp_downsample \
+        --tune_vision_tower True \
+        --tune_mm_projector True \
+        --tune_language_model True \
+        --mm_vision_select_layer -2 \
+        --mm_use_im_start_end False \
+        --mm_use_im_patch_token False \
+        --image_aspect_ratio resize \
+        --bf16 True \
+        --output_dir $OUTPUT_DIR/model \
+        --num_train_epochs 1 \
+        --per_device_train_batch_size $PER_DEVICE_TRAIN_BATCH_SIZE \
+        --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
+        --evaluation_strategy no \
+        --save_strategy steps \
+        --save_steps 100 \
+        --save_total_limit 1 \
+        --learning_rate 1e-4 \
+        --weight_decay 0. \
+        --warmup_ratio 0.03 \
+        --lr_scheduler_type cosine \
+        --logging_steps 1 \
+        --model_max_length 4096 \
+        --gradient_checkpointing True \
+        --dataloader_num_workers 16 \
+        --vflan_no_system_prompt True \
+        --report_to wandb
