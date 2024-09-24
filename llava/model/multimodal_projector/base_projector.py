4b521c145fb6038f8e070cc563ddaeaa7fea723f
Zhijian Liu <5782437+zhijian-liu@users.noreply.github.com>
2024-09-23 20:33:09 -0400

    Add `paligamma-siglip-448` (#250)

--- llava/model/multimodal_projector/base_projector.py
+++ llava/model/multimodal_projector/base_projector.py
@@ -62,6 +62,7 @@ class DownSampleBlock(nn.Module):
         if h % 2 == 1:
             x = torch.concat([x, torch.zeros((n, w, 1, c), dtype=x.dtype).to(x.device)], dim=2).contiguous()
             n, w, h, c = x.size()
+        x = x.contiguous()
         x = x.view(n, w, int(h / 2), int(c * 2))
         x = x.permute(0, 2, 1, 3).contiguous()
         x = x.view(n, int(h / 2), int(w / 2), int(c * 4))
